10dd54aeeb7fd798da931ae5b2eaf306ddb4b711
David Schmitz <david.schmitz.privat@gmail.com>
2018-12-10 19:36:40 +0100

    Added circleci build

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express from 'express'
+import expressHealthcheck from 'express-healthcheck'
+import errorHandler from 'errorhandler'
+
 const app = express()
 app.set('port', process.env.PORT || 3000)
 
 app.get('/hello', (req, res) => res.send('hello world'))
+
+app.get('/healthz', expressHealthcheck({
+  healthy: () => ({ status: 'ok' }),
+  test: () => 0
+}))
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler())
+}
 
 export default app
